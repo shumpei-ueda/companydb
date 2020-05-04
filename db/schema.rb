5806138c5dc6892685b7a4e3a2feb457166bd5f7
shumpei.ueda <shunnpei.soccer@gmail.com>
2020-05-04 17:51:06 +0900

    table add prefecture_id to city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_034256) do
+ActiveRecord::Schema.define(version: 2020_05_04_084924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_034256) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "prefecture_id"
   end
 
   create_table "companies", force: :cascade do |t|
