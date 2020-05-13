2b0e2f7343e3765ec3775e4f8b1d19ccdba0441f
FeLvi-zzz <34182539+FeLvi-zzz@users.noreply.github.com>
2020-05-13 15:40:33 +0900

    modify search logic

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -22,15 +22,15 @@ class CompaniesController < ApplicationController
     @sectors = Sector.all
     prefectures = params[:prefectures]
 
-    if @company_name.present?
-      @companies = DemoCompany.where('name like ?', "%#{@company_name.tr("a-zA-Z0-9&',.-", "ａ-ｚＡ-Ｚ０-９＆’，．－")}%")
-      @companies = @companies.where(prefecture_id: prefectures) if params[:prefectures].present?
-      @companies = @companies.where("prtimes_flag > 0") if @use_prtimes = !!params[:prtimes]
-      @companies = @companies.where("mynavi_flag > 0") if @use_mynavi = !!params[:mynavi]
-    end
-    @results = @companies
+    
+    @companies = DemoCompany
+    @companies = @companies.where('name like ?', "%#{@company_name.tr("a-zA-Z0-9&',.-", "ａ-ｚＡ-Ｚ０-９＆’，．－")}%") if @company_name.present?
+    @companies = @companies.where(prefecture_id: prefectures) if params[:prefectures].present?
+    @companies = @companies.where("prtimes_flag > 0") if @use_prtimes = !!params[:prtimes]
+    @companies = @companies.where("mynavi_flag > 0") if @use_mynavi = !!params[:mynavi]
 
-    @results ||= []
+    @results = @companies == DemoCompany || !@companies ? [] : @companies
+
     @count = @results.count
     render("companies/search_form")
 
